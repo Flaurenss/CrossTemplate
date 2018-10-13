03b79009e9714f925e36eeacf18ba769e58b2bae
Flaurenss <42294298+Flaurenss@users.noreply.github.com>
2018-10-13 21:58:56 +0200

    Fix null entry on configuration screen

---
+++ MvvmCross.Core/CustomTrigger/ValidationTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Template.Core.CustomTrigger
+{
+    public class ValidationTrigger : TriggerAction<Entry>
+    {
+        private string _prevValue = string.Empty;
+        protected override void Invoke(Entry entry)
+        {
+            if (entry.Text == "")
+            {
+                entry.Text = "0";
+                return;
+            }
+            //int n;
+            //var isNumeric = int.TryParse(entry.Text, out n);
+
+            //if (!string.IsNullOrWhiteSpace(entry.Text) && (entry.Text.Length > 9 || !isNumeric))
+            //{
+            //    entry.Text = _prevValue;
+            //    return;
+            //}
+
+            //_prevValue = entry.Text;
+        }
+    }
+}
